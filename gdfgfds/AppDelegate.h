b873575297eb7f5a29708bc4a07188e3451c88fe
maijianle <jvlstudio@gmail.com>
2015-10-27 18:29:39 +0800

    dfsafdsafdsa

---
+++ gdfgfds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1Alert
+//
+//  Created by LiuBliss on 15/10/24.
+//  Copyright © 2015年 LiuBliss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
